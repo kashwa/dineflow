41f6d84ad03f38389628e4c3c36a26459ccd71fb
kashwa <kashwa80@gmail.com>
2025-10-18 18:44:32 +0300

    ✅ Fix Factory for tests

---
+++ database/factories/TenantFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tenant>
+ */
+class TenantFactory extends Factory
+{
+    protected $model = Tenant::class;
+
+    public function definition(): array
+    {
+        return [
+            'name'   => $this->faker->company(),
+            'slug' => $this->faker->unique()->slug()
+        ];
+    }
+}
